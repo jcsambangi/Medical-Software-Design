8ebcb6e24afdc18aeb77a9b45ef33f2690c5b110
Suyash Kumar <suyashkumar2003@gmail.com>
2018-02-26 22:32:26 -0500

    Address review comments

--- Lectures/intro_web_services/call_apis/call_adpl.py
+++ Lectures/intro_web_services/call_apis/call_adpl.py
@@ -8,18 +8,20 @@ def get_sites():
     """
     Issues a GET request to adpl.suyash.io/api/sites to fetch the list
     of ADPL sites.
+
     :returns list: list of site keys/identifiers 
     """
     r = requests.get("http://adpl.suyash.io/api/sites")
     site_array = r.json() # parses the JSON response from the GET request into a python entity (in this case an array)
-    return r.json()
+    return site_array
 
 def get_today_temps(site_key):
     """
     Issues a GET request to the adpl server to fetch 1 day of temperatures from the
     specified site_key.
+
     :param str site_key: the site identifier
-    :param list: List of temperature record dicts for the past day
+    :returns list: List of temperature record dicts for the past day
     """
     r = requests.get("http://adpl.suyash.io/api/list/{0}/1".format(site_key))
     return r.json()
